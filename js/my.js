a38c6653e469c53283160fb3296288a88f1d4b3c
郭飞 <me@guofei.site>
2023-08-21 13:46:54 +0000

    update

---
+++ js/my.js
@@ -0,0 +1,33 @@
+// 类似这样的：
+// - 第1栏
+//     - 1
+//     - 2
+// - 第2栏
+//     - 3
+
+function Md2Col(md){
+	md = md.trim();
+	let res = '';
+	let col_num = 0;
+	md.split('\n').forEach(i => {
+		if (i.startsWith('-')) {
+			col_num += 1;
+			res += `    </ul>\n</div>\n<div class="column-undefined"><h3>${i.slice(2).trim()}</h3>\n    <ul>\n`;
+		} else {
+			res += `        <li>${i.trim().slice(2)}</li>\n`;
+		}
+	});
+	res += '    </ul>\n</div>';
+	res = res.slice(17);
+	res = res.replace(/column-undefined/g, 'column' + col_num);
+	return res;
+}
+
+
+document.querySelectorAll('div').forEach(div => {
+    if (div.classList.contains('my-columns')) {
+		let md=div.textContent;
+		const res=Md2Col(md);
+		div.innerHTML=res;
+    }
+});
