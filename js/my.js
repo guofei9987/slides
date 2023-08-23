c305760b3fd9dcc9de90156e4e054f6f99d0ee9a
郭飞 <me@guofei.site>
2023-08-23 11:48:59 +0000

    u

--- js/my.js
+++ js/my.js
@@ -12,13 +12,13 @@ function Md2Col(md){
 	md.split('\n').forEach(i => {
 		if (i.startsWith('-')) {
 			col_num += 1;
-			res += `    </ul>\n</div>\n<div class="column-undefined"><h3>${i.slice(2).trim()}</h3>\n    <ul>\n`;
+			res += `    </ul></div>\n</div>\n<div class="box column-undefined"><div class="title">${i.slice(2).trim()}</div>\n    <div class="content"><ul>\n`;
 		} else {
 			res += `        <li>${i.trim().slice(2)}</li>\n`;
 		}
 	});
 	res += '    </ul>\n</div>';
-	res = res.slice(17);
+	res = res.replace(`    </ul></div>\n`,``);
 	res = res.replace(/column-undefined/g, 'column' + col_num);
 	return res;
 }
@@ -28,6 +28,7 @@ document.querySelectorAll('div').forEach(div => {
     if (div.classList.contains('my-columns')) {
 		let md=div.textContent;
 		const res=Md2Col(md);
+		console.log(res);
 		div.innerHTML=res;
     }
 });
